5db2272f8fd41b2cbda12f3d01e64b418314d94c
Sebastian Senf <contact@mustardamus.com>
2016-11-11 12:45:14 +0100

    styled all pictures view, make toggle buttons work

--- src/js/pictures.js
+++ src/js/pictures.js
@@ -1,7 +1,23 @@
 /* global $ */
 
 $(() => {
+  let $cities = $('#cities')
+  let $toggleCities = $('#view-toggle-cities')
+  let $togglePictures = $('#view-toggle-pictures')
+
   $('.city-cover').on('click', function () {
     $(this).parent().toggleClass('is-active')
+  })
+
+  $toggleCities.on('click', function () {
+    $cities.removeClass('only-pictures')
+    $toggleCities.addClass('is-active')
+    $togglePictures.removeClass('is-active')
+  })
+
+  $togglePictures.on('click', function () {
+    $cities.addClass('only-pictures')
+    $togglePictures.addClass('is-active')
+    $toggleCities.removeClass('is-active')
   })
 })
