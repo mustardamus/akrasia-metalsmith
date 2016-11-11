b59e7a71c11518a4ec6709815c7b53b71416f873
Sebastian Senf <contact@mustardamus.com>
2016-11-11 14:39:14 +0100

    simple goto top link when too far in the pictures page

--- src/js/pictures.js
+++ src/js/pictures.js
@@ -2,22 +2,34 @@
 
 $(() => {
   let $cities = $('#cities')
+  let $sidebar = $('#sidebar')
   let $toggleCities = $('#view-toggle-cities')
   let $togglePictures = $('#view-toggle-pictures')
+  let citiesMenuHeight = $('#cities-menu').height()
 
   $('.city-cover').on('click', function () {
     $(this).parent().toggleClass('is-active')
   })
 
-  $toggleCities.on('click', function () {
+  $toggleCities.on('click', () => {
     $cities.removeClass('only-pictures')
     $toggleCities.addClass('is-active')
     $togglePictures.removeClass('is-active')
   })
 
-  $togglePictures.on('click', function () {
+  $togglePictures.on('click', () => {
     $cities.addClass('only-pictures')
     $togglePictures.addClass('is-active')
     $toggleCities.removeClass('is-active')
+  })
+
+  $(window).on('scroll', () => {
+    let top = window.pageYOffset || document.documentElement.scrollTop
+
+    if (top > citiesMenuHeight) {
+      $sidebar.addClass('top-link')
+    } else {
+      $sidebar.removeClass('top-link')
+    }
   })
 })
