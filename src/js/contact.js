9e1030aa4af2a72af634be244e42842fe793382c
Sebastian Senf <contact@mustardamus.com>
2016-11-20 23:08:24 +0100

    titles

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,6 +1,6 @@
 /* global $ */
 
-const validateEmail = function(email) {
+const validateEmail = function (email) {
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   return re.test(email)
 }
